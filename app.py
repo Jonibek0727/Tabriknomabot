fe8af3eef4a9b833fa6c0674376ee6ec59941c71
Jonibek0727 <88550962+Jonibek0727@users.noreply.github.com>
2022-03-05 18:12:34 +0500

    disable sql
    sql baza o'chirildi

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from aiogram import executor
 # from pprint import pprint as print
 
-from loader import dp, db
+from loader import dp
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -12,10 +12,10 @@ async def on_startup(dispatcher):
     # Birlamchi komandalar (/star va /help)
     await set_default_commands(dispatcher)
     # Ma'lumotlar bazasini yaratamiz:
-    try:
-        db.create_table_users()
-    except Exception as err:
-        pass
+    # try:
+    #     db.create_table_users()
+    # except Exception as err:
+    #     pass
 
     # Bot ishga tushgani haqida adminga xabar berish
     try:
--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,5 +1,5 @@
 from . import help
 from . import start
-from . import admin
+# from . import admin
 from . import commands
 from . import echo
