cc50bb136e2334eaeed1fd7a16a33d62bcbf0049
Jonibek0727 <88550962+Jonibek0727@users.noreply.github.com>
2022-03-05 17:41:30 +0500

    update config
    config -> env. fayli to'g'irlandi

--- data/config.py
+++ data/config.py
@@ -1,12 +1,17 @@
-from environs import Env
+# from environs import Env
 
-# environs kutubxonasidan foydalanish
-env = Env()
-env.read_env()
+# # environs kutubxonasidan foydalanish
+# env = Env()
+# env.read_env()
 
-# .env fayl ichidan quyidagilarni o'qiymiz
-BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
-ADMINS = env.list("ADMINS")  # adminlar ro'yxati
-IP = env.str("ip")  # Xosting ip manzili
+# # .env fayl ichidan quyidagilarni o'qiymiz
+# BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+# ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+# IP = env.str("ip")  # Xosting ip manzili
 
 GROUP_CHAT_ID = -1001704364861
+
+import os 
+BOT_TOKEN = str(os.environs.get("BOT_TOKEN"))
+ADMINS = list(os.environs.get("ADMINS"))
+IP = str(os.environs.get("ip"))
